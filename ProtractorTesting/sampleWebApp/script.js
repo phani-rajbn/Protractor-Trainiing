ec41370654dd34abe9d5243a6aefac41dde55aee
phani-rajbn <35284581+phani-rajbn@users.noreply.github.com>
2018-01-16 10:42:30 +0530

    Latest

--- ProtractorTesting/sampleWebApp/script.js
+++ ProtractorTesting/sampleWebApp/script.js
@@ -1,8 +1,7 @@
-var app = angular.module("myApp",[]);
-app.controller('myController', function ($scope){
+angular.module('myApp',[]).controller('myController', function($scope){
     $scope.helloTo = {};
     $scope.helloTo.title ="World, AngularJs Program"
-    $scope.person ={}
-    $scope.person.name ="";
-    $scope.animals =['Tiger Mogli','Samba, the Lion','Molly the elephant' ]
+    $scope.person = {}
+    $scope.person.name = "Rajesh";
+    $scope.animals = ['Tiger Mogli','Samba, the Lion','Molly the elephant']
 })
